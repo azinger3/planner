3fc266294700c893703ce791cdf63f3b8dc967dd
Rob Azinger <azinger3@gmail.com>
2016-09-29 16:13:17 -0400

    reorganized examples

--- example/basic-jsx-precompile/example.js
+++ example/basic-jsx-precompile/example.js
@@ -2,10 +2,9 @@ var ExampleApplication = React.createClass({
   render: function() {
     var elapsed = Math.round(this.props.elapsed  / 100);
     var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
-    var message =
-      'React has been successfully running for ' + seconds + ' seconds.';
+    var message = 'React has been successfully running for ' + seconds + ' seconds.';
 
-    return <p>{message}</p>;
+    return ({message});
   }
 });
 
