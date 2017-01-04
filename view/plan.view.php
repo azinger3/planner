6ac3a997ae10b8adb7a2d91a2d04e75348acd6ee
Rob Azinger <azinger3@gmail.com>
2017-01-03 22:45:06 -0500

    no message

--- view/plan.view.php
+++ view/plan.view.php
@@ -8,7 +8,21 @@
         ?>
 
         <style>
+            .budgetMonthHeader {
+                font-size: 28px; 
+                margin-bottom: 20px;
+                margin-top: -5px;
+            }
 
+            .pager li > a, .pager li > span {
+                background-color: #3498db !important;
+                margin: 5px !important;
+            }
+
+            h1 {
+                margin-top: 15px !important;
+                margin-bottom: 48.5px !important;
+            }
         </style>
     </head>
     <body>
@@ -23,17 +37,36 @@
             
             <!--Page Header START-->
             <div class="page-header">
-                <div class="row">
+                <div class="row text-center">
+                    <div class="col-md-2">
+                    </div>
+                    <div class="col-md-8">
+                        <ul class="pager">
+                            <li class="previous"><a href="#">&larr;&nbsp;&nbsp;August 2017</a></li>
+                            <li class="budgetMonthHeader">September 2017</li>
+                            <li class="next"><a href="#">October 2017&nbsp;&nbsp;&rarr;</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2">
+                    </div>
                 </div>
             </div>
             <!--Page Header END-->
 
             <!--Content START-->
             <div class="row">
-
+                <div class="col-md-1">
+                </div>
+                <div class="col-md-10" style="margin-top: 10%;">
+                    <div class="jumbotron text-center">
+                        <h1>Start Planning for September</h1>
+                        <p><a href="#" class="btn btn-info btn-lg btn-block">Go</a></p>
+                    </div>
+                </div>
+                <div class="col-md-1" >
+                </div>
             </div>
             <div class="row">
-
             </div>
             <!--Content END-->
 
