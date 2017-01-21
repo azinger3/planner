d2cec2a945157266bac0b4485926e346eebe8667
Rob Azinger <azinger3@gmail.com>
2017-01-21 14:53:38 -0500

    no message

--- include/header.php
+++ include/header.php
@@ -3,10 +3,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-        <link rel="apple-touch-icon" sizes="57x57" href="../image/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="../image/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="../image/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="../image/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/image/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/image/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/image/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/image/apple-icon-144x144.png" />
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
