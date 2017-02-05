8a588064bb29fe2b95379537bc12b1acd362f3da
Rob Azinger <azinger3@gmail.com>
2017-02-05 16:21:04 -0500

    no message

--- view/transaction.view.php
+++ view/transaction.view.php
@@ -149,7 +149,7 @@
                                     <input type="text" id="uxDescription" class="form-control" placeholder="Description" />
                                 </div>
                                 <div class="form-group">
-                                    <input type="number" id="uxAmount" class="form-control" placeholder="Amount $" pattern="\d*" />
+                                    <input type="text" id="uxAmount" class="form-control" placeholder="Amount $" pattern="[0-9]*" />
                                 </div>
                                 <div class="form-group">
                                     <div id="uxBudgetCategoryOptionAdd">
