6435a72acb5245d0bbbdcfaa0988cd4679fa36d8
Rob Azinger <azinger3@gmail.com>
2017-03-19 16:45:36 -0400

    no message

--- view/plan.view.php
+++ view/plan.view.php
@@ -164,7 +164,7 @@
                 left: 0;
                 right: 0;
                 text-align: center;
-                z-index: 9999;
+                z-index: 1049;
             }
 
             #uxBudgetMonthScroll {
